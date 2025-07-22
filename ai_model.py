5e1e6495d636eb50a2fb7db2c8d537d3e94e0844
faixanpervaix <faixanpervaix@gmail.com>
2025-07-22 19:24:36 +0200

    adding company brochure project

---
+++ ai_model.py
@@ -0,0 +1,53 @@
+"""
+This module defines the AiModel class, which serves as a base class for AI models.
+It includes attributes for model type, name, API key, and configuration, along with methods for
+preparing the model and providing string representations.
+"""
+
+from typing import Dict, Any
+
+class AiModel:
+    OLLAMA: str = "ollama"
+    OPEN_AI: str = "open_ai"
+
+    def __init__(
+        self,
+        type: str = "ollama",
+        model_name: str = "llama3.1:8b",
+        api_key: str = "http://localhost:11434/api/chat",
+        config: Dict[str, Any] = dict(),
+    ):
+        self.type = type
+        self.model_name = model_name
+        self.api_key = api_key
+        self.config = config
+        self._prepare_model()
+
+    def _prepare_model(self) -> None:
+        """
+        Prepares the model for use.
+        This method can be overridden by subclasses to implement specific model preparation logic.
+        """
+        match self.type:
+            case AiModel.OLLAMA:
+                pass
+            case _:
+                pass
+
+    def _prepare_ollama(self) -> None:
+        
+    def __repr__(self):
+        """
+        Returns a string representation of the AiModel instance.
+        This is useful for debugging and logging purposes.
+        :return: A string representation of the AiModel instance.
+        """
+        return f"Model(type={self.type}, model_name={self.model_name}, api_key={self.api_key}, config={self.config})"
+    
+    def __str__(self):
+        """
+        Returns a concise string representation of the AiModel instance.
+        This is useful for displaying the model information in a user-friendly format.
+        :return: A concise string representation of the AiModel instance.
+        """
+        return f"Model(type={self.type}, model_name={self.model_name}"
